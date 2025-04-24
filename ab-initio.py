8cae988f6077ed44df2a6fdea21ec6beff273eba
mariapascualfont <maria.pascual.font@estudiantat.upc.edu>
2025-04-24 14:35:17 +0200

    Update ab-initio.py

--- ab-initio.py
+++ ab-initio.py
@@ -1,3 +1,3 @@
 """
-Ab-initio method
+Ab-initio methods use gene content information and signals in the genomic sequence, that in this case will be used to predict the intron areas of a RNA sequence.
 """
