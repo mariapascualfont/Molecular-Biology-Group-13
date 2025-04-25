66cc389a13d15b64d968c5dab63d81ed6d656e7e
mariapascualfont <maria.pascual.font@estudiantat.upc.edu>
2025-04-25 19:39:51 +0200

    Update extract_seq.sh

--- extract_seq.sh
+++ extract_seq.sh
@@ -1,1 +1,2 @@
+#This code is used to get the sequences of the geneid prediction from its locations and the original fasta, using bedtools
 bedtools getfasta -fi contig_13.fa -bed geneid_locations.txt -fo geneid_13.fa 
