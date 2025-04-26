842fa706eafe4041c0d73e5b58d9c0a190233a3d
mireiafs <mireia.fabregas77@gmail.com>
2025-04-26 11:47:41 +0200

    Update homology-based.sh

--- homology-based.sh
+++ homology-based.sh
@@ -1,2 +1,7 @@
 #Align coding_13 with the superoxide dismutase [Cu-Zn] [Danio aesculapii] 
 exonerate -m p2g --showtargetgff -q danio_aesculapii.fa -t contig_13.fa
+
+#Select only exons features and save it in a gff
+exonerate -m p2g --showtargetgff -q danio_aesculapii.fa -t contig_13.fa -S F | egrep -w exon > contig_13.exonerate.gff
+
+#Using bedtools to get the exon sequences in fasta fromat
