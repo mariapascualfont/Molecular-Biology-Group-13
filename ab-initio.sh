59ac6c6a892cf6d72359802e2ca786e69b09307f
mariapascualfont <maria.pascual.font@estudiantat.upc.edu>
2025-04-26 10:03:41 +0200

    Update and rename extract_seq.sh to ab-initio.sh

--- ab-initio.sh
+++ ab-initio.sh
@@ -1,2 +1,3 @@
+#Shell script for ab-initio methods
 #This code is used to get the sequences of the geneid prediction from its locations and the original fasta, using bedtools
 bedtools getfasta -fi contig_13.fa -bed geneid_locations.txt -fo geneid_13.fa 
