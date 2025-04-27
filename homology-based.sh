3cbc4c8127edadd252c3b638f743744aa8e5747e
weilarm <mieke.weiler@estudiantat.upc.edu>
2025-04-27 14:25:04 +0200

    Update homology-based.sh

--- homology-based.sh
+++ homology-based.sh
@@ -1,44 +1,47 @@
+#The following shell script guides through the process from the alignment of contig_13 and (the gene coding for) the species protein
+#We conducted the shell script for the species respectively, as the commands remain the same.
+#The commands are based on the species Danio Aesculapii. All files are in the same working directory
+#For the other species we just replaced the species in the name of the files
+
 #Align coding_13 with the superoxide dismutase [Cu-Zn] [Danio aesculapii] 
 exonerate -m p2g --showtargetgff -q danio_aesculapii.fa -t contig_13.fa
 
 #Select only exons features and save it in a gff
-exonerate -m p2g --showtargetgff -q danio_aesculapii.fa -t contig_13.fa -S F | egrep -w exon > contig_13.exonerate.gff
+exonerate -m p2g --showtargetgff -q danio_aesculapii.fa -t contig_13.fa -S F | egrep -w exon > danio_aesculapii.exonerate.gff
 
 #Using bedtools to get the exon sequences in fasta fromat
-bedtools getfasta -fi danio_aesculapii.fa -bed contig_13.exonerate.gff > exonerate_cotntig_133.fa
+#bedtools getfasta -fi danio_aesculapii.fa -bed contig_13.exonerate.gff > exonerate_cotntig_133.fa
 
-# Made in google colab
-from google.colab import files
-uploaded = files.upload()
 
 # Show the first 2 lines of the contig_13.fa file
-!head -n 2 /content/contig_13.fa
+# !head -n 2 /content/contig_13.fa
 # Show the first 2 lines of the protein_hit.fa file
-!head -n 2 /content/protein_hit.fa
+# !head -n 2 /content/protein_hit.fa
 
 # Install samtools if it's not already installed
-!apt-get install samtools
+sudo apt install samtools
 
 # Generate the index for contig_13.fa
-!samtools faidx /content/contig_13.fa
+samtools faidx contig_13.fa
 
 # Verify that the index has been created correctly
-!ls -l /content/contig_13.fa.fai
+ls -l contig_13.fa.fai
 
 # Run Exonerate to align the protein to the genomic sequence
-!exonerate -m p2g --showtargetgff -q /content/protein_hit.fa -t /content/contig_13.fa -S F > exonerate_output.gff
+# !exonerate -m p2g --showtargetgff -q /content/protein_hit.fa -t /content/contig_13.fa -S F > exonerate_output.gff
 
 # Filter the exons from the GFF file
-!egrep -w exon exonerate_output.gff > exonerate_exons.gff
+# !egrep -w exon exonerate_output.gff > exonerate_exons.gff
 
 # Install bedtools if it's not already installed
-!apt-get install bedtools
+# !apt-get install bedtools
 
 # Extract the exon sequences using bedtools
-!bedtools getfasta -fi /content/contig_13.fa -bed exonerate_exons.gff > predicted_exons.fa
+bedtools getfasta -fi contig_13.fa -bed danio_aesculapii.exonerate.gff >exonerate_danio_aesculapii.fa
+# !bedtools getfasta -fi /content/contig_13.fa -bed exonerate_exons.gff > predicted_exons.far
 
 # Clean the file so that the sequences are in a single line
-!sed -e '2,$s/>.*//' predicted_exons.fa | grep -v '^$' > predicted_singleline.fa
+sed -e '2,$s/>.*//' exonerate_danio_aesculapii.fa | grep -v '^$' >exonerate_danio_aesculapii_single_line.fa
 
 # Using bedtools to get the exon sequences in fasta format
-bedtools getfasta -fi danio_aesculapii.fa -bed contig_13.exonerate.gff > exonerate_contig_13.fa
+#bedtools getfasta -fi danio_aesculapii.fa -bed contig_13.exonerate.gff > exonerate_contig_13.fa
