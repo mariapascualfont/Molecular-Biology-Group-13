7ff00593a40abbb394ae8b7b2896ab97b4ea0459
mariapascualfont <maria.pascual.font@estudiantat.upc.edu>
2025-04-27 16:15:06 +0200

    Update ab-initio.sh

--- ab-initio.sh
+++ ab-initio.sh
@@ -1,3 +1,9 @@
 #Shell script for ab-initio methods
 #This code is used to get the sequences of the geneid prediction from its locations and the original fasta, using bedtools
 bedtools getfasta -fi contig_13.fa -bed geneid_locations.gff -fo geneid_13.fa 
+
+#This code is used to merge the exons of GeneID in one single document
+echo ">geneid_13"\ > geneid_13_singleline.fa 
+sed -e 's/>contig_13:281-389//' -e 's/>contig_13:4961-5033//' geneid_13.fa | grep -v '^$' >> geneid_13_singleline.fa
+
+
