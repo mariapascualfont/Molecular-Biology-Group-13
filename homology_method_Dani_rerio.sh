a32ae40e2c4b7fec1c566854d221526597bf4079
weilarm <mieke.weiler@estudiantat.upc.edu>
2025-04-27 02:24:24 +0200

    Update homology_method_Dani_rerio.sh

--- homology_method_Dani_rerio.sh
+++ homology_method_Dani_rerio.sh
@@ -7,7 +7,12 @@ exonerate -m p2g --showtargetgff -q Zebrafish_contig13.fa -t contig_13.fa
 #select only exon features
 exonerate -m p2g --showtargetgff -q Zebrafish_contig13.fa -t contig_13.fa -S F| egrep -w exon > Zebrafish.exonerate.gff
 
-#now we extract the exons and store them in a new fasta file
+#we extract the exons and store them in a new fasta file
 #in our case the bedtools command did not work right away, so we installed and used samtools faidx 
 #samtools faidx creates an index that allows bedtools to access specific region of the fasta file (contig_13.fa) faster
+sudo apt install samtools
+samtools faidx contig_13.fa
 bedtools getfasta -fi contig_13.fa -bed Zebrafish.exonerate.gff >exonerate_Zebrafish.fa
+
+#we concatenate the exons into a single line
+sed -e '2,$s/>.*//' exonerate_Zebrafish.fa |grep -v '^$' >exonerate_Zebrafish_single_line.fa
