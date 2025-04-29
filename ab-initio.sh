7bd184448e54d0d2b8d6154e16f29a6153f9cd33
mariapascualfont <maria.pascual.font@estudiantat.upc.edu>
2025-04-29 11:01:23 +0200

    Update ab-initio.sh

--- ab-initio.sh
+++ ab-initio.sh
@@ -6,7 +6,9 @@ bedtools getfasta -fi contig_13.fa -bed geneid_locations.gff -fo geneid_13.fa
 echo ">geneid_13"\ > geneid_13_singleline.fa 
 sed -e 's/>contig_13:281-389//' -e 's/>contig_13:4961-5033//' geneid_13.fa | grep -v '^$' >> geneid_13_singleline.fa
 
-#This code is used to put all predictions together in a single file
-cat fgenesh_13.fa geneid_13_singleline.fa > contig_13_allpredicted.fa
+#This code is used to put all predictions together in a single file to run a T Coffee with just the ab-initio methods
+cat fgenesh_13.fa geneid_13_singleline.fa > contig_13_abinitio.fa
 
+#This code is used to put Fgenesh prediction with the homology based prediction to run a second T Coffee
+cat fgenesh_13.fa > contig_13_allpredicted.fa
 
